dd5fa20f969ebd7d228875d3083c420333fb036e
mariamartinez44 <mc.martinezm1@uniandes.edu.co>
2025-03-13 18:20:37 -0500

    ola

--- manejadorEventos/models.py
+++ manejadorEventos/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from manejadorPacientes.models import Paciente
+from manejadorEventos.models import Evento
 
 class EventoMedico(models.Model):
     fecha = models.DateField()
     motivo = models.TextField()
-    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name="eventos_medicos")
+    paciente = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name="eventos_medicos")
 
     def __str__(self):
         return f"Evento el {self.fecha} - {self.paciente.nombre}"
