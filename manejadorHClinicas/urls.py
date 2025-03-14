6180792c7917b94ec4ac49161c37fda6252c007b
mariamartinez44 <mc.martinezm1@uniandes.edu.co>
2025-03-13 19:30:38 -0500

    wii

--- manejadorHClinicas/urls.py
+++ manejadorHClinicas/urls.py
@@ -5,5 +5,5 @@ from .views import lista_historias_clinicas, historias_clinicas_view, historia_c
 urlpatterns = [
     path('historias_clinicas/', lista_historias_clinicas, name='lista_historias_clinicas'),
     path('historias_clinicas/', historias_clinicas_view, name='historias_clinicas_list'),
-    path('pacientes/<int:pk>/', historia_clinica_view, name='get_historia_clinicas'),
+    path('historias_clinicas/<int:pk>/', historia_clinica_view, name='get_historia_clinicas'),
 ]
