7a1734b587b6296af8e8e1b0ac2d666e12a93e30
WilliamBayona <w.bayona@uniandes.edu.co>
2025-03-13 22:46:06 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('manejadorEventos.urls')),
     path('', include('manejadorHClinicas.urls')),
     path('', include('manejadorPacientes.urls')),
+    path('health-check/', views.healthCheck),
 ]
